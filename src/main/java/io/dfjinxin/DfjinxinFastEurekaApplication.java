ef22dc972d54ca501b29ae71f86713ebc9250c58
HDLR <492587402@qq.com>
2019-05-05 10:07:49 +0800

    spring-cloud-eureka

---
+++ src/main/java/io/dfjinxin/DfjinxinFastEurekaApplication.java
@@ -0,0 +1,15 @@
+package io.dfjinxin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DfjinxinFastEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DfjinxinFastEurekaApplication.class, args);
+    }
+
+}
